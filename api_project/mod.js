08c58366323ecee06ccd4d88bb13a940876b472a
Nathan Banaszak <56935011+Zubinatub@users.noreply.github.com>
2021-06-24 04:51:19 -0400

    Made this edit users properly

--- api_project/mod.js
+++ api_project/mod.js
@@ -15,12 +15,74 @@ router.get('/mod/:userid', function (req, res){
                         if(!rows){
                                 res.sendStatus(404);
                         } else {
-                                res.send(`<form>
-                                <label for="username">Username:</label><br>
-                                <input type="text" id="username" name="username" value= "` + rows.firstName + `"><br>
-                                <label for="pwd">Password:</label><br>
-                                <input type="password" id="pwd" name="pwd">
-                                </form>`);
+                                res.send(`<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<script src="https://unpkg.com/vue@next"></script>
+		<title>Demo</title>
+	</head>
+	<body>
+
+		<header>
+			<ul>
+				<li><a href="http://35.39.165.238/index.html">Posts</a></li>
+				<li><a href="http://35.39.165.238/users.html">Users</a></li>
+			</ul>
+		</header>
+
+		<form id='putform'>
+            <b>Edit User ` + rows.id +`</b><br>
+            <label for="firstname">First Name:</label><br>
+            <input type="text" id="firstname" name="firstname" value= "` + rows.firstName + `"><br>
+            <label for="lastname">Last Name:</label><br>
+            <input type="text" id="lastname" name="lastname" value= "` + rows.lastName + `"><br>
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email" value= "` + rows.email + `"><br>
+            <label for="password">Password:</label><br>
+            <input type="text" id="password" name="password" value= "` + rows.password + `"><br>
+            <button v-on:click="enterform">Enter</button>
+        </form>
+	</body>
+<style>
+</style>
+	<script>
+        const EventHandling = {
+        methods: {
+            enterform(){
+                var fname = document.getElementById('firstname').value;
+                var lname = document.getElementById('lastname').value;
+                var mail = document.getElementById('email').value;
+                var pass = document.getElementById('password').value;
+                try{
+				console.log(fname + ', ' + lname);
+			fetch('http://35.39.165.238:3500/users/` + rows.id + `',
+				{
+					headers: {
+						'Accept': 'application/json',
+					         'Content-Type': 'application/json'
+					},
+					method: 'PUT',
+					body:JSON.stringify({ 'firstName': fname,
+						'lastName': lname,
+                        'email': mail,
+                        'password': pass
+					})
+				})
+				.then(response => response.json())
+                .then(data => console.log(data));
+			} catch(e){
+				console.log(e);
+			}
+			return false;
+            }
+        }
+        }
+
+        Vue.createApp(EventHandling).mount('#putform')
+	</script>
+</html>`);
                         }
                 }
         });
